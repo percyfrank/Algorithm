663654fc7e43dfa078b577e173e3d1679ddcfe1a
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-07-20 21:20:55 +0900

    148897_스타트와 링크(#2)

---
+++ BOJ/psygibag/14889_스타트와 링크(#2).py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+for _ in range(n):
+    arr.append(list(map(int,input().split())))
+
+tmp1 = []
+result = float('inf')
+
+def solve(idx):
+    global result
+    if len(tmp1) == n//2:
+        tmp2 = []
+        start = 0
+        link = 0
+        for i in range(n):
+            if i not in tmp1:
+                tmp2.append(i)
+        for i in range(n//2):
+            for j in range(n//2):
+                start += arr[tmp1[i]][tmp1[j]]
+                link += arr[tmp2[i]][tmp2[j]]
+        result = min(result, abs(start-link))
+        return
+
+    for i in range(idx,n):
+        if i not in tmp1:
+            tmp1.append(i)
+            solve(i+1)
+            tmp1.pop()
+
+solve(0)
+print(result)
