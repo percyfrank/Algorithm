20f86a0949739ee1e6e8e54e1f681680666f2ec1
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-07-31 21:30:09 +0900

    10431_줄세우기(#1)

---
+++ BOJ/ses9928/10431_줄세우기.py
@@ -0,0 +1,29 @@
+
+p = int(input())
+start = 1
+
+for _ in range(p):
+    arr = list(map(int,input().split()))
+    if not arr:
+        break
+    arr = arr[1:]
+    tmp = []
+    tmp.append(arr[0])
+    cnt = 0
+    for i in range(1,20):
+        if arr[i] > tmp[-1]:
+            tmp.append(arr[i])
+        else:
+            for j in range(len(tmp)):
+                if arr[i] < tmp[j]:
+                    cnt += len(tmp) - j
+                    break
+            tmp.append(arr[i])
+            tmp.sort()
+
+    print(start,cnt)
+    start += 1
+
+
+
+
