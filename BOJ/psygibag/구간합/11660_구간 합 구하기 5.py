a7ee310aa28aaa107c96f0d37a4937472748214c
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-08-02 22:05:32 +0900

    11660_구간 합 구하기(DP 성공)

--- BOJ/psygibag/구간합/11660_구간 합 구하기 5.py
+++ BOJ/psygibag/구간합/11660_구간 합 구하기 5.py
@@ -3,20 +3,16 @@ n,m = map(int,input().split())
 
 chart = []
 for _ in range(n):
-    arr = list(map(int,input().split()))
-    tmp = []
-    start = 0
-    tmp.append(0)
-    for i in range(len(arr)):
-        tmp.append(start + arr[i])
-        start += arr[i]
-    chart.append(tmp)
+    chart.append(list(map(int,input().split())))
+
+dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + chart[i-1][j-1]
 
 for _ in range(m):
     x,y,X,Y = map(int,input().split())
-    result = 0
-    for i in range(x-1,X):
-        result += chart[i][Y] - chart[i][y-1]
+    result = dp[X][Y] - dp[X][y-1] - dp[x-1][Y] + dp[x-1][y-1]
     print(result)
 
 
