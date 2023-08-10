b6ae8b1456323f3c0a81c31c9ebe77e865958085
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-08-10 22:19:36 +0900

    GBC(소프티어) - 시간 더 오래걸림

--- Softeer/Level2/GBC.py
+++ Softeer/Level2/GBC.py
@@ -1,22 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 
-start = 1
-total = [0] * 101
-for _ in range(n):
-    sector, limit = map(int, input().split())
-    for i in range(start, start+sector):
-        total[i] = limit
-    start += sector
+sec_limit = [list(map(int, input().split())) for _ in range(n)]
+test = [list(map(int, input().split())) for _ in range(m)]
 
-s_start = 1
+speed_limit = []
+for sector, limit in sec_limit:
+    speed_limit += [limit for _ in range(sector)]
+
+speed_test = []
+for sector, t_limit in test:
+    speed_test += [t_limit for _ in range(sector)]
+
 MAX = 0
-for _ in range(m):
-    sec, speed = map(int, input().split())
-    for i in range(s_start, s_start+sec):
-        MAX = max(MAX, speed-total[i])
-    s_start += sec
+for i in range(100):
+    MAX = max(MAX, speed_test[i]-speed_limit[i])
 
 print(MAX)
