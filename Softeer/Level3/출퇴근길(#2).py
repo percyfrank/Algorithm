50e76599201494845ee5c29c9129e70404c08d9f
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-08-21 01:24:05 +0900

    출퇴근길(소프티어) - bfs성공

--- Softeer/Level3/출퇴근길(#2).py
+++ Softeer/Level3/출퇴근길(#2).py
@@ -12,42 +12,33 @@ for _ in range(m):
 
 s,t = map(int,input().split())
 
-def bfs(end,isVisit,adj):
+def bfs(start, isVisit, adj):
+    q = deque()
+    q.append(start)
+    isVisit[start] = 1
     while q:
         curr = q.popleft()
-        if curr == end:
-            continue
-        for i in range(len(adj[curr])):
-            target = adj[curr][i]
+        for target in adj[curr]:
             if isVisit[target] == 0:
                 isVisit[target] = 1
                 q.append(target)
 
-q = deque()
-q.append(s)
 isVisited1 = [0] * (n+1)
-isVisited1[s] = 1
-bfs(t,isVisited1,adj)
+isVisited1[t] = 1
+bfs(s, isVisited1, adj)
 
-q.clear()
-q.append(t)
 isVisited2 = [0] * (n+1)
-isVisited2[t] = 1
-bfs(s,isVisited2,adj)
+isVisited2[s] = 1
+bfs(t, isVisited2, adj)
 
-q.clear()
-q.append(s)
 isVisited3 = [0] * (n+1)
-bfs(t,isVisited3,adjR)
+bfs(s, isVisited3, adjR)
 
-q.clear()
-q.append(t)
 isVisited4 = [0] * (n+1)
-bfs(s,isVisited4,adjR)
+bfs(t, isVisited4, adjR)
 
 cnt = 0
 for i in range(1,n+1):
-    if isVisited1[i] == 1 and isVisited2[i] == 1 and isVisited3[i] == 1 and isVisited4[i] == 1:
+    if isVisited1[i] and isVisited2[i] and isVisited3[i] and isVisited4[i]:
         cnt += 1
 print(cnt-2)
-
