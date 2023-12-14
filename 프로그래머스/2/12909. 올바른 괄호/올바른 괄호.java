33ba947ec6d2bfde0b4a2dc2d810a29a504231db
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-12-14 21:16:37 +0900

    [level 2] Title: 올바른 괄호, Time: 18.28 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        
+        
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        if (!stack.empty()) {
+            return false;
+        }
+
+        return answer;
+    }
+}
