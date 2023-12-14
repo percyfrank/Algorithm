d01cd6d8064f3da8054591698b52b0294508cd90
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-12-14 23:23:02 +0900

    [level 2] Title: 피보나치 수, Time: 2.31 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        
+        int[] fibo = new int[100001];
+        fibo[1] = 1;
+        
+        for(int i=2; i<=n; i++) {
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+        }
+        
+        return fibo[n];
+    }
+}
