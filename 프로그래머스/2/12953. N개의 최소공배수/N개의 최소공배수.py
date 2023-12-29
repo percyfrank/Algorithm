8dd15aab6627f152705add8a3ed7d865796003b0
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-12-30 01:43:17 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,23 +1,12 @@
 def solution(arr):
     
-#     a,b = arr[0],arr[1]
-#     while b > 0:
-#         a,b = b,a%b
+    def gcd(a,b):
+        while b > 0:
+            a,b = b,a%b
+        return a
     
-#     answer = a
-#     for num in arr:
-#         answer *= num // a
+    answer = arr[0]
+    for num in arr:
+        answer = (answer * num) / gcd(answer,num)
     
-    lcs = sorted(arr)[-1]
-    while True:
-        cnt = 0
-        for num in arr:
-            if lcs % num != 0:
-                lcs += 1
-                break
-            else:
-                cnt += 1
-        if cnt == len(arr):
-            return lcs
-    
-    # return answer
+    return answer
