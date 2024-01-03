9942a43271e0127d349e0fffdc5c9e7c7ee02b31
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-03 21:38:11 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,11 +1,9 @@
 from collections import Counter
 
 def solution(want, number, discount):
-    answer = 0
     
-    cnt = dict()
-    for w,n in zip(want,number):
-        cnt[w] = n
+    cnt = {i:j for i,j in zip(want,number)}
+    answer = 0
     
     for i in range(len(discount)-9):
         tmp = Counter(discount[i:i+10])
