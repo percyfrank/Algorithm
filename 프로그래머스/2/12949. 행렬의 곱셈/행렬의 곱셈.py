bfbdf97576d0e884b9d0e90670127b32649a26f3
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-03 23:15:51 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.98 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    
+    row = len(arr1)
+    col = len(arr2[0])
+    answer = [[0 for _ in range(col)] for _ in range(row)]
+    
+    for i in range(row):
+        for j in range(col):
+            for k in range(len(arr1[0])):
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+            
+    return answer
