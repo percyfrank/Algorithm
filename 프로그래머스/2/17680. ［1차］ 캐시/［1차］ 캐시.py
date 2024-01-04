746bde9225b2e05d78ca23ef5502c98d7f3d1476
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-04 22:04:01 +0900

    [level 2] Title: [1차] 캐시, Time: 0.63 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    
+    answer = 0
+    q = deque(maxlen=cacheSize)
+    for city in cities:
+        city = city.lower()
+        if city in q:
+            answer += 1
+            q.remove(city)
+            q.append(city)
+        else:
+            answer += 5
+            q.append(city)
+    
+    return answer
