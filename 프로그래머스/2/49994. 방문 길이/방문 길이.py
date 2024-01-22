d3962c8b608c7d3313dbebe697a6c031abc3f755
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-22 21:30:54 +0900

    [level 2] Title: 방문 길이, Time: 0.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,22 +1,15 @@
 def solution(dirs):
     
     tmp = set()
+    d = {'U' : (0,1),'D' : (0,-1),'R' : (1,0),'L' : (-1,0)}
+    
     x,y = 0,0
     for dir in dirs:
-        
-        if dir == "U":
-            nx,ny = x, y+1
-        elif dir == "D":
-            nx,ny = x, y-1
-        elif dir == "R":
-            nx,ny = x+1, y
-        elif dir == "L":
-            nx,ny = x-1, y
+        nx,ny = x+d[dir][0], y+d[dir][1]
             
         if -5 <= nx <= 5  and -5 <= ny <= 5:
             tmp.add((x,y,nx,ny))
             tmp.add((nx,ny,x,y))
             x,y = nx,ny    
 
-    
     return len(tmp) // 2
