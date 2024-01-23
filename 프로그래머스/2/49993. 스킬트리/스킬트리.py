b366ef2242033da6c115a8ca1b32f8e066fb75b0
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-23 21:23:44 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,24 +1,13 @@
 def solution(skill, skill_trees):
     
-    prev = {skill[i]:i+1 for i in range(len(skill))}
-    for i in range(len(skill_trees)):
-        tmp = []
-        for st in skill_trees[i]:
-            if st in prev.keys():
-                tmp.append(prev[st])
-        skill_trees[i] = tmp
-    
     answer = 0
     for skill_tree in skill_trees:
-        start = 1
-        flag = True
+        tmp = ''
         for st in skill_tree:
-            if start == st:
-                start += 1
-            else:
-                flag = False
-                break
-        if flag:
+            if st in skill:
+                tmp += st
+        
+        if tmp == skill[0:len(tmp)]:
             answer += 1
     
     return answer
