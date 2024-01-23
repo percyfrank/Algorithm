04fe2fb911f2bf2b65d9e8d2b993c42d0598569f
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-23 23:21:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -10,8 +10,8 @@ def solution(topping):
         cnt.add(t)
         total[t] -= 1
         if total[t] <= 0:
-            del total[t]
-        
+            # del total[t]
+            total.pop(t)
         if len(cnt) == len(total):
             answer += 1
 
