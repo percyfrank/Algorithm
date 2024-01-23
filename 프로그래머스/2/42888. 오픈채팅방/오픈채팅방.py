e87d1ba790e9a1cee4b6528103c2b7e2687d7c11
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-23 22:27:51 +0900

    [level 2] Title: 오픈채팅방, Time: 71.35 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -2,22 +2,15 @@ def solution(record):
     
     answer = []
     name = {}
+    order = {"Leave" : "님이 나갔습니다.","Enter":"님이 들어왔습니다."}
     for rc in record:
         tmp = rc.split(" ")
-        if tmp[0] == "Leave":
-            answer.append([tmp[1],"Leave"])
-        else:
-            command,id,nickname = tmp[0],tmp[1],tmp[2]
-            if command == "Enter":
-                answer.append([id,"Enter"])
-                name[id] = nickname
-            elif command == "Change":
-                name[id] = nickname
+        if tmp[0] != "Leave":
+            name[tmp[1]] = tmp[2]
     
-    order = {"Leave" : "님이 나갔습니다.","Enter":"님이 들어왔습니다."}
-    result = []
-    for id, comm in answer:
-        if id in name.keys():
-            result.append(name[id]+order[comm])
+    for rc in record:
+        tmp = rc.split(" ")
+        if tmp[0] != "Change":
+            answer.append(name[tmp[1]]+order[tmp[0]])
     
-    return result
+    return answer
