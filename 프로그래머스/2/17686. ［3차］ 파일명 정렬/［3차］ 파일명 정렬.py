84c15c6bb01a2de126c771a2c6c43806e17e6000
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-01-24 22:51:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.29 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -7,19 +7,17 @@ def solution(files):
         for i in range(len(file)):
             if file[i].isdigit():
                 number += file[i]
+                if len(number) == 5:
+                    tail = file[i+1:]
+                    break
                 flag = True
             elif not flag:
                 head += file[i]
-            else:
+            else:        
                 tail = file[i:]
                 break
-                
-            # if len(number) == 5:
-            #     answer.append([head,number,file[i:]])
-            #     break
-        
         answer.append([head,number,tail])
-    
+
     answer.sort(key=lambda x :(x[0].lower(),int(x[1])))
     
     return [''.join(i) for i in answer]
