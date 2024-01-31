703c65c1298af62d277f3433250ce284fcd2d4fb
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-01 00:36:50 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 415.90 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,11 +1,13 @@
 def solution(numbers):
+    
     answer = [-1] * len(numbers)
     stack_idx = []
     
     for i in range(len(numbers)):
-        while stack_idx and numbers[stack_idx[-1]] < numbers[i]:
-            answer[stack_idx.pop()] = numbers[i]
+        while stack_idx and stack_idx[-1][1] < numbers[i]:
+            idx,num = stack_idx.pop()
+            answer[idx] = numbers[i]
         
-        stack_idx.append(i)
+        stack_idx.append((i,numbers[i]))
     
     return answer
