15c32be4cd21d7b896c66c3ee5a589105d917eae
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-03 00:11:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    
+    q = deque([0]*bridge_length)
+    curr = 0
+    while q:
+        answer += 1
+        curr -= q.popleft()
+        
+        if truck_weights:
+            if curr + truck_weights[0] <= weight:
+                curr += truck_weights[0]
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
+
+    return answer
+
+# 아래 코드는 sum 함수로 인해 시간 초과
+# while q:
+#     answer += 1
+#     q.popleft()
+#
+#     if truck_weights:
+#         if sum(q) + truck_weights[0] <= weight:
+#             q.append(truck_weights.pop(0))
+#         else:
+#             q.append(0)
