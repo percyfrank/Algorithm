222e0256f010ae9a42cfbe15719f55a7667bab0f
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-06 23:36:54 +0900

    [level 2] Title: 삼각 달팽이, Time: 320.58 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645) 
 
 ### 성능 요약
 
-메모리: 42.9 MB, 시간: 352.25 ms
+메모리: 42.9 MB, 시간: 320.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 23:5:2
+2024년 2월 2일 23:36:54
 
 ### 문제 설명
 
--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,27 +1,24 @@
 def solution(n):
-
+    
     def is_range(x,y):
         return x>=0 and y>=0 and x<n and y<n
-
-    arr = [[0 for j in range(1, i+1)] for i in range(1, n+1)]
-    dirs = {0 : (0,1), 1: (1,0), 2: (-1,-1)}
+    
+    arr = [[0 for j in range(i+1)] for i in range(n)]
+    dirs = {0 : (0,1), 1 : (1,0), 2 : (-1,-1)}
     x,y,dir = 0,0,1
-    arr[0][0] = 1
-    total = n*(n+1) // 2
-
-    for num in range(2,total+1):
-        nx,ny = x + dirs[dir][0] , y + dirs[dir][1]
+    arr[x][y] = 1
+    
+    for num in range(2, n*(n+1)//2 + 1):
+        nx, ny = x + dirs[dir][0], y + dirs[dir][1]
         if not is_range(nx,ny) or arr[nx][ny] != 0:
             dir = (dir+2) % 3
-        nx,ny = x + dirs[dir][0] , y + dirs[dir][1]
+        nx, ny = x + dirs[dir][0], y + dirs[dir][1]
         arr[nx][ny] = num
-        x,y = nx,ny
+        x, y = nx, ny
     
     answer = []
-    for i in arr:
-        for j in i:
-            if j:
-                answer.append(j)
-    
+    for row in arr:
+        for num in row:
+            answer.append(num)
+            
     return answer
-
