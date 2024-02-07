5cf802d4d56be52a12a9df3bd32dbd13a1e95ff3
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-07 22:50:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -3,7 +3,7 @@ def solution(number, k):
     answer = []
     
     for num in number:
-        while answer and answer[-1] < num and k > 0:
+        while answer and int(answer[-1]) < int(num) and k > 0:
             answer.pop()
             k -= 1
         answer.append(num)
