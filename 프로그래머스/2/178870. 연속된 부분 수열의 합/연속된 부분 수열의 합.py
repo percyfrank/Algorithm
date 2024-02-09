d939c5e55e4b468f57df92b8b888e71e5174b87e
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-10 01:14:41 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,50 +1,20 @@
-# def solution(sequence, k):
-#     answer = []
-    
-#     prefix_sum = [0]
-#     for i in range(len(sequence)):
-#         prefix_sum.append(prefix_sum[-1]+sequence[i])
-    
-    
-
-#     for i in range(1,len(sequence)+1):
-        
-#         start = 0
-#         end = len(sequence)-i
-        
-#         while start <= end:
-#             mid = (start + end) // 2
-#             print(mid)
-#             # if sequence[mid+i] - sequence[mid] == k:
-#             #     return [start, start+i-1]
-#             elif sequence[mid+i] - sequence[mid] >= k:
-#                 end = mid
-#             else:
-#                 start = mid + 1
-                
-#         if sequence[mid+i] - sequence[mid] == k:
-#             return [start, start+i-1]
-#             
-    # return answer
-    
 def solution(sequence, k):
-    L = len(sequence)
-    sequence = [0] + sequence
+    
+    prefix_sum = [0]
+    for i in range(len(sequence)):
+        prefix_sum.append(prefix_sum[-1]+sequence[i])    
 
-    for i in range(1, L+1):
-        sequence[i] = sequence[i-1] + sequence[i]
-
-    for l in range(1, L + 1):
-
-        s = 0
-        e = L - l
-
-        while s < e:
-            m = (s + e) // 2
-            if sequence[m+l] - sequence[m] >= k:
-                e = m
+    for i in range(1, len(sequence)+1):
+        
+        start = 0
+        end = len(sequence) - i
+        
+        while start < end:
+            mid = (start + end) // 2
+            if prefix_sum[mid+i] - prefix_sum[mid] >= k:
+                end = mid
             else:
-                s = m + 1
-
-        if sequence[s+l] - sequence[s] == k:
-            return [s, s + l - 1]
+                start = mid + 1
+        
+        if prefix_sum[start+i] - prefix_sum[start] == k:
+            return [start, start+i-1]
