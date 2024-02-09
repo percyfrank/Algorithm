2a3b7d8e514192410790d09695733404008608be
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-09 21:29:47 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,20 +1,50 @@
-def solution(sequence, k):
+# def solution(sequence, k):
+#     answer = []
     
-    answer = []
-    right = 0
-    subtotal = 0
+#     prefix_sum = [0]
+#     for i in range(len(sequence)):
+#         prefix_sum.append(prefix_sum[-1]+sequence[i])
     
-    for left in range(len(sequence)):
+    
+
+#     for i in range(1,len(sequence)+1):
         
-        while subtotal < k and right < len(sequence):
-            subtotal += sequence[right]
-            right += 1
-            
-        if subtotal == k:
-            answer.append([left,right-1, right-left-1])
-            
-        subtotal -= sequence[left]
+#         start = 0
+#         end = len(sequence)-i
+        
+#         while start <= end:
+#             mid = (start + end) // 2
+#             print(mid)
+#             # if sequence[mid+i] - sequence[mid] == k:
+#             #     return [start, start+i-1]
+#             elif sequence[mid+i] - sequence[mid] >= k:
+#                 end = mid
+#             else:
+#                 start = mid + 1
+                
+#         if sequence[mid+i] - sequence[mid] == k:
+#             return [start, start+i-1]
+#             
+    # return answer
     
-    answer.sort(key=lambda x:(x[2],x[0]))
-    
-    return [answer[0][0],answer[0][1]]
+def solution(sequence, k):
+    L = len(sequence)
+    sequence = [0] + sequence
+
+    for i in range(1, L+1):
+        sequence[i] = sequence[i-1] + sequence[i]
+
+    for l in range(1, L + 1):
+
+        s = 0
+        e = L - l
+
+        while s < e:
+            m = (s + e) // 2
+            if sequence[m+l] - sequence[m] >= k:
+                e = m
+            else:
+                s = m + 1
+
+        if sequence[s+l] - sequence[s] == k:
+            return [s, s + l - 1]
