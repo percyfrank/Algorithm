05384fb51797ef96ad12338787631f19f9b9216a
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-17 00:15:34 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.50 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -2,25 +2,37 @@ from collections import deque
 
 def solution(n, wires):
     
+    def dfs(start):
+        cnt = 1
+        visited[start] = True
+        
+        for i in tmp_wire[start]:
+            if not visited[i]:
+                visited[i] = True
+                cnt += dfs(i)
+                
+        return cnt
+    
+#     def bfs(start):
+#         q = deque()
+#         q.append(start)
+#         visited[start] = True
+#         cnt = 1
+        
+#         while q:
+#             start = q.popleft()            
+#             for i in tmp_wire[start]:
+#                 if not visited[i]:
+#                     q.append(i)
+#                     visited[i] = True
+#                     cnt += 1
+#         return cnt
+
     tmp_wire = [[] for _ in range(n+1)]
     for a,b in wires:
         tmp_wire[a].append(b)
         tmp_wire[b].append(a)
-    
-    def bfs(start):
-        q = deque()
-        q.append(start)
-        visited[start] = True
-        cnt = 1
-        
-        while q:
-            start = q.popleft()            
-            for i in tmp_wire[start]:
-                if not visited[i]:
-                    q.append(i)
-                    visited[i] = True
-                    cnt += 1
-        return cnt
+    print(tmp_wire)
 
     answer = n
     for v1,v2 in wires:
@@ -29,7 +41,8 @@ def solution(n, wires):
         
         cnt = 0
         visited = [False for _ in range(n+1)]  
-        answer = min(answer,abs(bfs(v1)-bfs(v2)))
+        # answer = min(answer,abs(bfs(v1)-bfs(v2)))
+        answer = min(answer,abs(dfs(v1)-dfs(v2)))
         
         tmp_wire[v1].append(v2)
         tmp_wire[v2].append(v1)
