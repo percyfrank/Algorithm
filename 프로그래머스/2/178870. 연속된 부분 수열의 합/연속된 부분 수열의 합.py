49c6be4d1b30abef6e32e8c2885f1b414da5224d
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-17 15:22:36 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,20 +1,19 @@
 def solution(sequence, k):
-    
-    prefix_sum = [0]
-    for i in range(len(sequence)):
-        prefix_sum.append(prefix_sum[-1]+sequence[i])    
 
-    for i in range(1, len(sequence)+1):
+    n = len(sequence) 
+    end, subtotal = 0, 0
+    answer = []
+    for start in range(n):
         
-        start = 0
-        end = len(sequence) - i
+        while subtotal < k and end < n:
+            subtotal += sequence[end]
+            end += 1
         
-        while start < end:
-            mid = (start + end) // 2
-            if prefix_sum[mid+i] - prefix_sum[mid] >= k:
-                end = mid
-            else:
-                start = mid + 1
+        if subtotal == k:
+            answer.append([start,end-1])
         
-        if prefix_sum[start+i] - prefix_sum[start] == k:
-            return [start, start+i-1]
+        subtotal -= sequence[start]
+    
+    answer.sort(key=lambda x:(x[1]-x[0],x[0]))
+    
+    return answer[0]
