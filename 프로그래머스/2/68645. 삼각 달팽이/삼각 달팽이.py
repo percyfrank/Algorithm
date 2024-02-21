1c2efbb2fe34e36c72d6a2cc36dc7733c902a5c7
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-21 21:28:08 +0900

    [level 2] Title: 삼각 달팽이, Time: 333.02 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -3,22 +3,23 @@ def solution(n):
     def is_range(x,y):
         return x>=0 and y>=0 and x<n and y<n
     
-    arr = [[0 for j in range(i+1)] for i in range(n)]
+    arr = [[0 for _ in range(n)] for _ in range(n)]
     dirs = {0 : (0,1), 1 : (1,0), 2 : (-1,-1)}
     x,y,dir = 0,0,1
     arr[x][y] = 1
-    
-    for num in range(2, n*(n+1)//2 + 1):
-        nx, ny = x + dirs[dir][0], y + dirs[dir][1]
+
+    for i in range(2,n*(n+1)//2+1):
+        nx,ny = x+dirs[dir][0], y+dirs[dir][1]
         if not is_range(nx,ny) or arr[nx][ny] != 0:
             dir = (dir+2) % 3
-        nx, ny = x + dirs[dir][0], y + dirs[dir][1]
-        arr[nx][ny] = num
-        x, y = nx, ny
-    
+        nx,ny = x+dirs[dir][0], y+dirs[dir][1]
+        arr[nx][ny] = i
+        x,y = nx,ny
+
     answer = []
-    for row in arr:
-        for num in row:
-            answer.append(num)
-            
+    for i in arr:
+        for j in i:
+            if j != 0:
+                answer.append(j)  
+    
     return answer
