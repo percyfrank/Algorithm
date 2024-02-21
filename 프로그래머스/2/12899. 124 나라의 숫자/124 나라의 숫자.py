0cd55db14ca202b2e58fd2f1819429aa9d42b592
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-21 22:01:17 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,10 +1,12 @@
 def solution(n):
-    answer = ''
-    rule = "124"
     
+    answer = ''
     while n > 0:
-        n -= 1
-        answer += rule[n%3]
-        n //= 3
+        q,r = divmod(n,3)
+        if r == 0:
+            r = 4
+            q -= 1
+        answer += str(r)
+        n = q
     
     return answer[::-1]
