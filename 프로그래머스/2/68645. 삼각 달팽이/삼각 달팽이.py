6f20a9900643f6f25e7bd6f1bb2b71f258eb64c1
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-21 21:30:09 +0900

    [level 2] Title: 삼각 달팽이, Time: 382.57 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -3,7 +3,7 @@ def solution(n):
     def is_range(x,y):
         return x>=0 and y>=0 and x<n and y<n
     
-    arr = [[0 for _ in range(n)] for _ in range(n)]
+    arr = [[0 for j in range(1, i+1)] for i in range(1, n+1)]
     dirs = {0 : (0,1), 1 : (1,0), 2 : (-1,-1)}
     x,y,dir = 0,0,1
     arr[x][y] = 1
