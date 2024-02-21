c8f65532bc4db939d4842a5924064ef09e905261
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-21 23:01:56 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,12 +1,12 @@
 def solution(storey):
-
     answer = 0
+    
     while storey > 0:
         storey,r = divmod(storey,10)
-        if r > 5 or (r == 5 and storey % 10 >= 5):
+        if r > 5 or (r == 5 and storey%10 >= 5):
             answer += 10 - r
             storey += 1
         else:
             answer += r
-            
+    
     return answer
