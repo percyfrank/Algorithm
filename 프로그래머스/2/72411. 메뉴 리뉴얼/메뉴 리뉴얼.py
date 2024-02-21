0964fce9a494dabd9da26682eb859e073450c625
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-02-21 22:35:50 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,15 +1,15 @@
 from itertools import combinations
 from collections import Counter
- 
+
 def solution(orders, course):
     
-    answer = []
+    answer = [] 
     for c in course:
         tmp = []
         for order in orders:
             tmp += combinations(sorted(order),c)
-        
+            
         tmp = Counter(tmp).most_common()
         answer += [key for key,value in tmp if value > 1 and value == tmp[0][1]]
         
-    return [''.join(data) for data in sorted(answer)]
+    return [''.join(data) for data in sorted(answer)] 
