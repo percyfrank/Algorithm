493f7be07d8e2d90aa7ae83d06340a69f44fd689
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-03-01 19:56:24 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,15 +1,14 @@
-from collections import deque
+
 from math import factorial
 
 def solution(n, k):
     
-    arr = deque([i for i in range(1,n+1)])
+    arr = [i for i in range(1,n+1)]
     answer = []
 
     while arr:
-        num = (k-1) // factorial(n-1)
-        answer.append(arr[num])
-        arr.remove(arr[num])
+        idx = (k-1) // factorial(n-1)
+        answer.append(arr.pop(idx))
         
         k %= factorial(n-1)
         n -= 1
