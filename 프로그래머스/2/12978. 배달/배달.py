f77e9a98b9eadf6a891ccd91032b8d7b2b35c225
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-03-13 22:19:23 +0900

    [level 2] Title: 배달, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,42 +1,28 @@
-def solution(N, road, K):
+import heapq
 
-    def get_smallest_node():
-        min_val = float('inf')
-        idx = 0
-        for i in range(1,N+1):
-            if distance[i] < min_val and not visited[i]:
-                min_val = distance[i]
-                idx = i
-        return idx
+def solution(N, road, K):
     
     def dijkstra(start):
-        distance[start] = 0
-        visited[start] = True
         
-        for k,v in maps[start].items():
-            distance[k] = v
-            
-        for i in range(N-1):
-            now = get_smallest_node()
-            visited[now] = True
-            
-            for node,dist in maps[now].items():
-                if distance[now] + dist < distance[node]:
-                    distance[node] = distance[now] + dist
-                            
-    visited = [False] * (N+1)
-    distance = [float('inf')] * (N+1)            
+        q = []
+        heapq.heappush(q,(start,0))
+        distance[start] = 0
+        
+        while q:
+            now, dist = heapq.heappop(q)
+                
+            for next, cost in maps[now].items():
+                if dist + cost < distance[next]:
+                    distance[next] = dist + cost
+                    heapq.heappush(q,(next,dist+cost))
+    
+    distance = [float('inf')] * (N+1)
     maps = [{} for _ in range(N+1)]
     for a,b,cost in road:
-        if b not in maps[a] or maps[a][b] > cost:
+        if b not in maps[a] or cost < maps[a][b]:
             maps[a][b] = cost
             maps[b][a] = cost
-            
+    
     dijkstra(1)
     
-    answer = 0
-    for i in range(1,N+1):
-        if distance[i] <= K:
-            answer += 1
-            
-    return answer
+    return len([i for i in range(1,N+1) if distance[i] <= K])
