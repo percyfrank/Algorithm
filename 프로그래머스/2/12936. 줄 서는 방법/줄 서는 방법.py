287267eb71d15cb94b885bc7c26e69db482d087a
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-05-01 21:35:12 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,16 +1,15 @@
-
 from math import factorial
 
 def solution(n, k):
     
     arr = [i for i in range(1,n+1)]
     answer = []
-
+    
     while arr:
         idx = (k-1) // factorial(n-1)
         answer.append(arr.pop(idx))
         
-        k %= factorial(n-1)
-        n -= 1
-        
+        k = k % factorial(n-1)
+        n = n-1    
+    
     return answer
