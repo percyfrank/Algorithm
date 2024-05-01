b16b70122917bba8403ec723fe8caf7abecf3edd
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-05-01 16:29:23 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,44 +1,45 @@
 from collections import deque
 
 def solution(board):
-        
+    
     def is_range(x,y):
         return x>=0 and y>=0 and x<n and y<m
-                
+
     def move(x,y,dir):
+        
         while True:
             x += dx[dir]
             y += dy[dir]
             if not is_range(x,y) or board[x][y] == "D":
                 break
-        
-        return x - dx[dir], y - dy[dir]
-    
-    def bfs(x,y,dis):
+                
+        return x-dx[dir],y-dy[dir]
+            
+    def bfs(x,y,distance):
         q = deque()
-        q.append((x,y,dis))
+        q.append((x,y,distance))
+        visited[x][y] = True
         
         while q:
-            x,y,dis = q.popleft()
+            x,y,distance = q.popleft()
             if board[x][y] == "G":
-                return dis
+                return distance
             for i in range(4):
                 nx,ny = move(x,y,i)
-                if (nx,ny) not in visited:
-                    visited.add((nx,ny))
-                    q.append((nx,ny,dis+1))
-
-        return -1
-                
-    answer = 0
-    n,m = len(board),len(board[0])
-    dx,dy = [0,1,0,-1],[1,0,-1,0]
-    visited = set()
+                if is_range(nx,ny) and not visited[nx][ny] and board[nx][ny] != "D":
+                    q.append((nx,ny,distance+1))
+                    visited[nx][ny] = True
         
+        return -1
+
+    answer = 0
+    dx,dy = [0,1,0,-1],[1,0,-1,0]
+    n,m = len(board), len(board[0])
+    visited = [[0 for _ in range(m)] for _ in range(n)]
     for i in range(n):
         for j in range(m):
             if board[i][j] == "R":
                 answer = bfs(i,j,0)
                 
-    return answer
     
+    return answer
