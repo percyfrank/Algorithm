0897b16da0f5d1f826a33679ec69515a4348faf8
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-05-01 18:20:15 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -4,41 +4,40 @@ def solution(maps):
     
     def is_range(x,y):
         return x>=0 and y>=0 and x<n and y<m
-
-    def bfs(start,end):
+    
+    def bfs(start,end,dis):
         
-        visited = [[-1 for _ in range(m)] for _ in range(n)]
+        visited = [[0 for _ in range(m)] for _ in range(n)]
         q = deque()
-        q.append((start[0],start[1]))
-        visited[start[0]][start[1]] = 0
+        q.append((start[0],start[1],dis))
+        visited[start[0]][start[1]] = True
         
         while q:
-            x,y = q.popleft()
+            x,y,dis = q.popleft()
             if x == end[0] and y == end[1]:
-                return visited[x][y]
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if is_range(nx,ny) and maps[nx][ny] != "X" and visited[nx][ny] == -1:
-                    visited[nx][ny] = visited[x][y] + 1
-                    q.append((nx,ny))
-
+                return dis
+            for dx,dy in ((0,1),(1,0),(0,-1),(-1,0)):
+                nx = x + dx
+                ny = y + dy
+                if not is_range(nx,ny):
+                    continue
+                if maps[nx][ny] != "X" and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx,ny,dis+1))
         return -1            
-        
-    
-    n,m = len(maps), len(maps[0])
-    dx,dy = [0,1,0,-1],[1,0,-1,0]
-    start,lever,exit = [],[],[]
+
+    n,m = len(maps),len(maps[0])
+    start,lever,end = [],[],[]
     for i in range(n):
         for j in range(m):
             if maps[i][j] == "S":
                 start = [i,j]
-            if maps[i][j] == "L":
+            elif maps[i][j] == "L":
                 lever = [i,j]
-            if maps[i][j] == "E":
-                exit = [i,j]
-                
-    to_lever = bfs(start,lever)
-    to_exit = bfs(lever,exit)
+            elif maps[i][j] == "E":
+                end = [i,j]
     
-    return to_lever+to_exit if to_lever != -1 and to_exit != -1 else -1
+    to_lever = bfs(start,lever,0)
+    to_end = bfs(lever,end,0)
+    
+    return to_lever + to_end if (to_lever != -1 and to_end != -1) else -1
