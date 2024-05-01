e0c151e34bf5775b524f8f79345177e011cff9d7
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-05-01 23:53:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,16 +1,16 @@
 def solution(n, costs):
     
-    answer = 0
     costs.sort(key=lambda x:x[2])
     link = set([costs[0][0]])
+    answer = 0
     
-    while len(link) < n:
-        for start,end,cost in costs:
-            if start in link and end in link:
+    while len(link) != n:
+        for cost in costs:
+            if cost[0] in link and cost[1] in link:
                 continue
-            if start in link or end in link:
-                link.update([start,end])
-                answer += cost
+            if cost[0] in link or cost[1] in link:
+                link.update([cost[0],cost[1]])
+                answer += cost[2]
                 break
     
     return answer
