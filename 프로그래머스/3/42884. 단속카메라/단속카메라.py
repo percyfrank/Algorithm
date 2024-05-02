874cc787bb7bc58c9f4e25429982d1d7926bfb30
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-05-02 23:32:32 +0900

    [level 3] Title: 단속카메라, Time: 0.87 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,8 +1,8 @@
 def solution(routes):
     
     routes.sort(key=lambda x:x[1])
-    camera = routes[0][1]
-    answer = 1
+    camera = -30001
+    answer = 0
     
     for route in routes:
         if route[0] > camera:
