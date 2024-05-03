d771790159b9781bd514fe7c492300ae0f14ff56
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-05-03 22:57:19 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,39 +1,29 @@
-from collections import deque
-
 def solution(begin, target, words):
         
     def compare(a,b):
         
-        cnt = 0      
-        for i in range(n):
+        cnt = 0   
+        for i in range(len(begin)):
             if a[i] != b[i]:
                 cnt += 1
         
         return True if cnt == 1 else False
     
-    def dfs(idx,cnt):
+    def dfs(word,cnt):
+            
+        if word == target:
+            cnt_list.append(cnt)
+            return
         
-        q = deque()
-        q.append((idx,cnt))
-        visited[idx] = True
+        for i in range(len(words)):
+            if not visited[i] and compare(word,words[i]):
+                visited[i] = True
+                dfs(words[i],cnt+1)
+                visited[i] = False
         
-        while q:
-            idx,cnt = q.popleft()
-            if words[idx] == target:
-                return cnt
-            for i in range(m):
-                if not visited[i] and compare(words[idx],words[i]):
-                    q.append((i,cnt+1))
-                    visited[i] = True
-                    
-        return 0
+
+    visited = [False] * len(words)
+    cnt_list = []
+    dfs(begin,0)
         
-    answer = 0
-    n,m = len(begin),len(words)
-    visited = [False] * m
-    
-    for i in range(m):
-        if not visited[i] and compare(begin,words[i]):
-            answer = dfs(i,1)
-    
-    return answer
+    return min(cnt_list) if cnt_list else 0
