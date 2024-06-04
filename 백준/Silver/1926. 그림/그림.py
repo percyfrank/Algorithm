61dbb73aa434390aea2f16abd35b3accbec98c54
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-06-04 23:39:38 +0900

    [Silver I] Title: 그림, Time: 440 ms, Memory: 36976 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -19,26 +19,26 @@ def bfs(x, y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if is_range(nx, ny):
-                if pictures[nx][ny] == 1 and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    cnt += 1
-                    q.append((nx, ny))
+            if not is_range(nx, ny):
+                continue
+            if pictures[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                cnt += 1
+                q.append((nx, ny))
 
-    return cnt
+    res.append(cnt)
 
 
-n, m = tuple(map(int, input().split(" ")))
-pictures = [list(map(int, input().split(" "))) for _ in range(n)]
+n, m = map(int, input().split())
+pictures = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False for _ in range(m)] for _ in range(n)]
 dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
-
 res = []
+
 for i in range(n):
     for j in range(m):
         if pictures[i][j] == 1 and not visited[i][j]:
-            cnt = bfs(i, j)
-            res.append(cnt)
+            bfs(i, j)
 
 print(len(res))
 print(max(res)) if res else print(0)
