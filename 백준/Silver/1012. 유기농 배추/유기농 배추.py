5f4862727b014c1a2634bc66f9b9bfb206a36534
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-06-13 22:50:53 +0900

    [Silver II] Title: 유기농 배추, Time: 72 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,46 +1,43 @@
-from collections import deque
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
 
-def is_range(x,y):
-    return x>=0 and y>=0 and x<n and y<m
+def is_range(x, y):
+    return 0 <= x < m and 0 <= y < n
 
-def bfs(x,y):
-    global cnt
 
-    cnt += 1
+def bfs(x, y):
     q = deque()
-    q.append((x,y))
-    visited[x][y] = True
+    q.append((x, y))
+    maps[x][y] = 0
 
     while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if is_range(nx,ny) and not visited[nx][ny]:
-                if maps[nx][ny] == 1:
-                    visited[nx][ny] = True
-                    q.append((nx,ny))
-    return
+        x, y = q.popleft()
+        for (dx, dy) in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+            nx = x + dx
+            ny = y + dy
+            if not is_range(nx, ny):
+                continue
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = 0
+                q.append((nx, ny))
 
 
 t = int(input())
 for _ in range(t):
-    m,n,k = map(int,input().split())    # 가로(m) 10, 세로(n) 8
-    maps = [[0 for _ in range(m)] for _ in range(n)]
-    visited = [[False for _ in range(m)] for _ in range(n)]
+    m, n, k = map(int, input().split())
+    maps = [[0] * n for _ in range(m)]
     for _ in range(k):
-        y,x = map(int,input().split())
+        x, y = map(int, input().split())
         maps[x][y] = 1
 
     cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 1 and not visited[i][j]:
-                bfs(i,j)
+    for i in range(m):
+        for j in range(n):
+            if maps[i][j] == 1:
+                bfs(i, j)
+                cnt += 1
 
     print(cnt)
