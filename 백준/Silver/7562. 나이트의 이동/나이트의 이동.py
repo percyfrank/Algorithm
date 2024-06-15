19565e5452b84864bb7425ab10d504483561d45b
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-06-15 12:55:01 +0900

    [Silver I] Title: 나이트의 이동, Time: 1720 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def is_range(x, y):
+    return 0 <= x < l and 0 <= y < l
+
+
+def bfs(x, y, X, Y):
+    q = deque()
+    q.append((x, y))
+    chess[x][y] = 1
+
+    while q:
+        x, y = q.popleft()
+        if x == X and y == Y:
+            return chess[x][y]
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if not is_range(nx, ny):
+                continue
+            if not chess[nx][ny]:
+                chess[nx][ny] = chess[x][y] + 1
+                q.append((nx, ny))
+
+
+t = int(input())
+dx = [-1, -2, -2, -1, 1, 2, 2, 1]
+dy = [2, 1, -1, -2, -2, -1, 1, 2]
+for _ in range(t):
+    l = int(input())
+    chess = [[0] * l for _ in range(l)]
+    start_x, start_y = map(int, input().split())
+    target_x, target_y = map(int, input().split())
+    cnt = bfs(start_x, start_y, target_x, target_y)
+    print(cnt - 1)
