fb3c57fcf4c83388c7fefedd3684235bcf796137
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-06-15 12:58:34 +0900

    [Silver I] Title: 나이트의 이동, Time: 1596 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -8,14 +8,14 @@ def is_range(x, y):
     return 0 <= x < l and 0 <= y < l
 
 
-def bfs(x, y, X, Y):
+def bfs(x, y):
     q = deque()
     q.append((x, y))
     chess[x][y] = 1
 
     while q:
         x, y = q.popleft()
-        if x == X and y == Y:
+        if x == target_x and y == target_y:
             return chess[x][y]
         for i in range(8):
             nx = x + dx[i]
@@ -28,12 +28,12 @@ def bfs(x, y, X, Y):
 
 
 t = int(input())
-dx = [-1, -2, -2, -1, 1, 2, 2, 1]
-dy = [2, 1, -1, -2, -2, -1, 1, 2]
 for _ in range(t):
     l = int(input())
+    dx = [-1, -2, -2, -1, 1, 2, 2, 1]
+    dy = [2, 1, -1, -2, -2, -1, 1, 2]
     chess = [[0] * l for _ in range(l)]
     start_x, start_y = map(int, input().split())
     target_x, target_y = map(int, input().split())
-    cnt = bfs(start_x, start_y, target_x, target_y)
+    cnt = bfs(start_x, start_y)
     print(cnt - 1)
