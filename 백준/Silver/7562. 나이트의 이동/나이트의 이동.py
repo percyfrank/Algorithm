b3a734d9431647a48ba92a7ac58a03b8e8868332
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-06-16 21:13:51 +0900

    [Silver I] Title: 나이트의 이동, Time: 1312 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -3,37 +3,39 @@ from collections import deque
 
 input = sys.stdin.readline
 
+dx = [-1, -2, -2, -1, 1, 2, 2, 1]
+dy = [2, 1, -1, -2, -2, -1, 1, 2]
 
-def is_range(x, y):
-    return 0 <= x < l and 0 <= y < l
 
+def bfs(start, target, chess, l):
 
-def bfs(x, y):
     q = deque()
-    q.append((x, y))
-    chess[x][y] = 1
+    q.append(start)
+    chess[start[0]][start[1]] = 1
 
     while q:
         x, y = q.popleft()
-        if x == target_x and y == target_y:
+        if (x, y) == target:
             return chess[x][y]
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            if not is_range(nx, ny):
-                continue
-            if not chess[nx][ny]:
-                chess[nx][ny] = chess[x][y] + 1
-                q.append((nx, ny))
+            if 0 <= nx < l and 0 <= ny < l:
+                if not chess[nx][ny]:
+                    chess[nx][ny] = chess[x][y] + 1
+                    q.append((nx, ny))
 
 
-t = int(input())
-for _ in range(t):
-    l = int(input())
-    dx = [-1, -2, -2, -1, 1, 2, 2, 1]
-    dy = [2, 1, -1, -2, -2, -1, 1, 2]
-    chess = [[0] * l for _ in range(l)]
-    start_x, start_y = map(int, input().split())
-    target_x, target_y = map(int, input().split())
-    cnt = bfs(start_x, start_y)
-    print(cnt - 1)
+def main():
+    t = int(input())
+    for _ in range(t):
+        l = int(input())
+        chess = [[0] * l for _ in range(l)]
+        start = tuple(map(int, input().split()))
+        target = tuple(map(int, input().split()))
+        cnt = bfs(start, target, chess, l)
+        print(cnt - 1)
+
+
+if __name__ == "__main__":
+    main()
