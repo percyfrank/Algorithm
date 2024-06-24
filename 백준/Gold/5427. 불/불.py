b8de3e29f66573ac4fda86a6927d3e31ae20e6ec
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-06-24 22:56:05 +0900

    [Gold IV] Title: 불, Time: 2488 ms, Memory: 70264 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -3,40 +3,37 @@ from collections import deque
 
 input = sys.stdin.readline
 
+dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 def is_range(x,y):
     return 0 <= x < h and 0 <= y < w
 
 
 def fire_bfs():
-
     while fire:
-        x, y = fire.popleft()
-        for (dx, dy) in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        x, y, cnt = fire.popleft()
+        for (dx, dy) in dirs:
             nx = x + dx
             ny = y + dy
             if not is_range(nx, ny):
                 continue
-            if buildings[nx][ny] != "#" and not fire_visited[nx][ny]:
-                fire_visited[nx][ny] = fire_visited[x][y] + 1
-                fire.append((nx, ny))
-
+            if buildings[nx][ny] != "#" and not visited[nx][ny]:
+                visited[nx][ny] = cnt + 1
+                fire.append((nx, ny, cnt+1))
 
 def human_bfs():
-
     while human:
-        x, y = human.popleft()
-        for (dx, dy) in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        x, y, cnt = human.popleft()
+        for (dx, dy) in dirs:
             nx = x + dx
             ny = y + dy
             if not is_range(nx, ny):
-                print(human_visited[x][y])
+                print(cnt)
                 return
-            if fire_visited[nx][ny] and fire_visited[nx][ny] <= human_visited[x][y] + 1:
-                continue
-            if buildings[nx][ny] != "#" and not human_visited[nx][ny]:
-                human_visited[nx][ny] = human_visited[x][y] + 1
-                human.append((nx, ny))
+            if buildings[nx][ny] != "#":
+                if cnt + 1 < visited[nx][ny] or not visited[nx][ny]:
+                    visited[nx][ny] = -1
+                    human.append((nx, ny, cnt+1))
 
     print("IMPOSSIBLE")
     return
@@ -47,19 +44,17 @@ for _ in range(t):
     buildings = []
     for i in range(h):
         buildings.append(input().rstrip())
-    human_visited = [[0] * w for _ in range(h)]
-    fire_visited = [[0] * w for _ in range(h)]
+    visited = [[0] * w for _ in range(h)]
     human = deque()
     fire = deque()
 
     for i in range(h):
         for j in range(w):
-            if buildings[i][j] == "@":
-                human.append((i, j))
-                human_visited[i][j] = 1
-            elif buildings[i][j] == "*":
-                fire.append((i, j))
-                fire_visited[i][j] = 1
+            if buildings[i][j] == "*":
+                fire.append((i, j, 1))
+                visited[i][j] = 1
+            elif buildings[i][j] == "@":
+                human.append((i, j, 1))
 
     fire_bfs()
     human_bfs()
