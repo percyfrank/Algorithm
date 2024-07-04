c8dae4289f0ab37fcbd62ff60688898cdfbdb7a6
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-07-05 00:20:38 +0900

    [Gold II] Title: 보석 도둑, Time: 1440 ms, Memory: 110288 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,20 +1,20 @@
 import heapq
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
 jewels = [list(map(int, input().split())) for _ in range(n)]
+jewels = deque(sorted(jewels))
 bags = [int(input()) for _ in range(k)]
-jewels.sort()
 bags.sort()
 
 answer = 0
 tmp = []
 for bag in bags:
     while jewels and bag >= jewels[0][0]:
-        heapq.heappush(tmp, -jewels[0][1])
-        heapq.heappop(jewels)
-
+        m, v = jewels.popleft()
+        heapq.heappush(tmp, -v)
     if tmp:
         answer -= heapq.heappop(tmp)
 
