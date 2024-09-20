6766309bd408bfb335a3a05acf06fddde9dd03a4
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-09-21 01:19:59 +0900

    [Gold V] Title: 탑, Time: 2364 ms, Memory: 145880 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        List<Integer> towers = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            towers.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.reverse(towers);
+
+        Deque<int[]> stack = new ArrayDeque<>();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && stack.peekLast()[1] < towers.get(i)) {
+                int[] tmp = stack.pollLast();
+                int idx = tmp[0];
+                int prev = tmp[1];
+                ans[idx] = n - i;
+            }
+
+            stack.add(new int[]{i, towers.get(i)});
+        }
+
+
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(ans[i] + " ");
+        }
+
+    }
+}
