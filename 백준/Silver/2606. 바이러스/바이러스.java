26a318c7ae07028c2a3db7289ef36edf0f604a09
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-10-14 22:57:19 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    static int[][] graph;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
     static int n,m;
     static int cnt;
@@ -14,38 +14,33 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
 
-        graph = new int[n+1][n+1];
+        graph = new ArrayList[n+1];
+        for(int i=0; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
         visited = new boolean[n+1];
-        StringTokenizer st;
         for(int i=0; i<m; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = graph[y][x] = 1;
+            graph[x].add(y);
+            graph[y].add(x);
         }
 
-        System.out.println(bfs(1));
+        System.out.println(dfs(1));
 
     }
 
-    public static int bfs(int start) {
+    public static int dfs(int start) {
 
-        Queue<Integer> q = new LinkedList<>();
         visited[start] = true;
-        q.offer(start);
-
-        while (!q.isEmpty()) {
-            int tmp = q.poll();
-            for (int i=1; i<=n; i++) {
-                if(graph[tmp][i] == 1 && !visited[i]) {
-                    q.offer(i);
-                    visited[i] = true;
-                    cnt++;
-                }
+        for(int x : graph[start]) {
+            if(!visited[x]) {
+                cnt++;
+                dfs(x);
             }
-
         }
+
         return cnt;
     }
-
 }
