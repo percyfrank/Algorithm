d6630b7ce6666d8bb0172cc78bf381c1b7ec2084
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-11-07 22:12:56 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 364.35 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -2,42 +2,43 @@ from collections import deque
 
 def solution(land):
     
-    def is_range(x,y):
-        return x>=0 and y>=0 and x<n and y<m
-    
     def bfs(x,y):
-        
+        nonlocal answer
+
         q = deque()
         q.append((x,y))
+        visited[x][y] = True
+        min_y, max_y = y, y
         cnt = 1
-        visited[x][y] = True        
-        min_y = max_y = y     
-        
         while q:
-            x,y = q.popleft()
-            min_y = min(y,min_y)
-            max_y = max(y,max_y)
+            x, y = q.popleft()
+            min_y = min(min_y,y)
+            max_y = max(max_y,y)
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if not is_range(nx,ny):
-                    continue
-                if land[nx][ny] == 1 and not visited[nx][ny]:
-                    visited[nx][ny] = 1
-                    q.append((nx,ny))
-                    cnt += 1
+                if 0 <= nx < n and 0 <= ny < m:
+                    if not visited[nx][ny] and land[nx][ny] == 1:
+                        q.append((nx,ny))
+                        visited[nx][ny] = True
+                        cnt += 1
         
         for i in range(min_y,max_y+1):
-            answer[i] += cnt
-
-    dx,dy = [0,1,0,-1],[1,0,-1,0]
-    n,m = len(land),len(land[0])
-    visited = [[0 for _ in range(m)] for _ in range(n)]
-    answer = [0] * m
+            answer[i] += cnt           
     
+    m, n = len(land[0]), len(land)    
+    dx, dy = [0,1,0,-1], [1,0,-1,0]
+    answer = [0] * m
+    visited = [[False] * m for _ in range(n)]    
     for i in range(n):
         for j in range(m):
-            if land[i][j] == 1 and not visited[i][j]:
+            if land[i][j] == 1 and not visited[i][j]:                
                 bfs(i,j)
 
     return max(answer)
+                
+
+ 
+    
+    
+    
