f5a90c2c5792fa678ebff55a8557d44dee8dc832
percykwon <85394884+percyfrank@users.noreply.github.com>
2024-11-08 22:42:19 +0900

    2024 상반기 금융결제원 2,3번 수정

--- 기업 코테 복기/금융결제원/2024 상반기 금융결제원_2번.py
+++ 기업 코테 복기/금융결제원/2024 상반기 금융결제원_2번.py
@@ -1,17 +1,9 @@
-
-def dfs(idx, arr):
-    if len(arr) == len(keys):
-        if arr not in total:
-            total.append(arr)
-        return
-    dfs(idx+1, arr+[False])
-    dfs(idx+1, arr+[True])
+from itertools import product
 
 
 keys = input()
 t = int(input())
-total = []
-dfs(0, [])
+total = list(product([False, True], repeat=len(keys)))
 
 answer = []
 for times in total:
@@ -23,15 +15,16 @@ for times in total:
             tmp.append([idx, idx+t+0.5])
 
     tmp.sort(key=lambda x: x[1])
-
     word = []
     for idx, time in tmp:
-        if word and keys[idx] == "-":
-            word.pop()
-        elif keys[idx] != "-":
+        if word and keys[idx] == '-':
+            word.pop()  # ["0","AB","B"]
+            # word.pop(0)   # ["0","A","AB","B"]
+        elif keys[idx] != '-':
             word.append(keys[idx])
 
     word = "".join(word)
+    print(word)
     if word == "":
         word = "0"
     if word not in answer:
@@ -40,19 +33,17 @@ for times in total:
 answer.sort()
 print(answer)
 
-
 #### 문제
-# keys = 문자열
+# keys = 문자열 <= 15
 # t = 시간
-# 문자열을 즉시 출력하거나 지연(t+0.5) 출력 가능
-# 즉시 출력 지연 출력은 언제든지 가능
-# 가능한 문자열 조합 정렬해서 모두 구하기
-# 문자열이 "-"인 경우 앞에서부터 문자열 1개 지우기
-# 문자열이 공백일 경우 "0" 출력
+# 문자열 즉시 출력 or 지연(t+0.5) 출력 가능
+# 각 문자마다 즉시 출력, 지연 출력 선택 횟수 제한 없음
+# 가능한 모든 문자열 조합 정렬된 상태로 구하기
+#### 문자열이 "-"인 경우 앞에서부터 문자열 1개 지우기 => 뒤에서부터 지우는 건지 불명확함
+# 문자열 전체가 공백일 경우 "0"으로 대체 출력
 
-# 입력
+#### 입출력
 # | keys | t | result |
-# | --- | --- | --- |
 # | "BA" | 1 | ["AB","BA"] |
 # | "YOLO" | 2 | ["LYOO","OLYO","OYLO","OYOL","YLOO","YOLO","YOOL"] |
 # | "-A-B" | 1 | ["0","A","AB","B"] |
--- 기업 코테 복기/금융결제원/2024 상반기 금융결제원_3번.py
+++ 기업 코테 복기/금융결제원/2024 상반기 금융결제원_3번.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
 
 
-def dfs(idx, cnt, total, arr):
+def backtracking(idx, cnt, total):
     global answer
 
     if cnt == row:
         answer = min(answer, total)
         return
 
-    # 내가 가지고 있는 카드랑 가은 갯수 만큼 점수 증가
+    # 내가 가지고 있는 카드랑 같은 갯수 만큼 점수 증가
     tmp = 0
     for card in cards[idx]:
         if card in my_card.keys():
@@ -18,14 +18,9 @@ def dfs(idx, cnt, total, arr):
         if not visited[idx][i]:
             visited[idx][i] = True
             my_card[cards[idx][i]] += 1
-            arr.append((idx, i))
-
-            dfs(idx + 1, cnt + 1, total + tmp, arr)
-
+            backtracking(idx + 1, cnt + 1, total + tmp)
             visited[idx][i] = False
             my_card[cards[idx][i]] -= 1
-            arr.pop()
-
 
 n = int(input())
 cards = [list(map(int, input().split())) for _ in range(n)]
@@ -33,40 +28,44 @@ answer = float('inf')
 row, col = len(cards), len(cards[0])
 visited = [[False for _ in range(col)] for _ in range(row)]
 my_card = defaultdict(int)
-dfs(0, 0, 0, [])
+backtracking(0, 0, 0)
 print(answer)
 
 #### 문제
 # n = 게임 턴 횟수
 # cards = 게임 턴 마다 뽑은 카드 조합
-# 매 턴마다 카드 조합을 새로 뽑고, 내가 가지고 카드랑 같은 갯수만큼 점수 증가
-# 매 턴마다 카드 조합을 뽑게되면 그 중 1장은 내 카드로 가져가야 함
+# 매 턴마다 카드 조합을 새로 뽑는다. 규칙은 다음과 같다.
+# 카드 조합 내의 각각의 숫자를 내가 가지고 있는 카드와 비교했을 때 같다면 같은 갯수만큼 점수 증가.
+# 그런 다음, 카드 조합 중 1장은 내 카드로 가져감.
 # 모든 턴을 지났을 때 얻은 점수의 최솟값 구하기
 
-#### 입력 조건
+#### 조건
 # cards의 길이 <= 10000
 # cards[i]의 길이 <= 100
 
+#### 입력
 # 5
-# 1 2 5 1 2 => 5
-# 3 1 3 3 2 => 1
-# 3 3 3 5 2 => 5
-# 2 2 2 4 3 => 3
-# 1 2 4 1 1 => 상관없음
+# 1 2 5 1 2 => 5            => +0
+# 3 1 3 3 2 => 1            => +0
+# 3 3 3 5 2 => 5            => +1
+# 2 2 2 4 3 => 3            => +0
+# 1 2 4 1 1 => 상관없음       => +3
 
-# 출력 : 4점
+#### 출력 : 4점
 
+#### 입력
 # 4
 # 2 10 => 10
 # 2 2  => 2
 # 2 2  => 2
 # 2 2  => 상관없음
 
-# 출력 : 6점
+#### 출력 : 6점
 
+#### 입력
 # 3
 # 3 1 2 3 => 1
 # 3 3 2 3 => 2
 # 3 3 3 3 => 상관없음
 
-# 출력 : 0점
+#### 출력 : 0점
