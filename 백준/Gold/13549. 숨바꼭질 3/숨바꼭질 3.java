1507f5c81aed5f630af721a831a560e8cbc50f38
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2024-11-24 23:47:31 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 108 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -22,7 +22,7 @@ public class Main {
                 if (i % 2 == 0) {
                     dp[i] = Math.min(dp[i],dp[i/2]);
                 } else {
-                    dp[i] = Math.min(dp[i],Math.min(dp[(i+1)/2]+1,dp[(i-1)/2]+1));
+                    dp[i] = Math.min(dp[i],dp[(i+1)/2]+1);
                 }
             }                
         }
